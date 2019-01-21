25e7263d4a3003fd09a4d0070b04ac7c31e8a718
SkyrinCHN <liuhai14@icloud.com>
2019-01-21 09:24:02 +0800

    `

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,62 +4,110 @@ Page({
    * 页面的初始数据
    */
   data: {
-    
+    list: [{
+      id: 1,
+      img_url: "http://localhost:3000/img/banner1.png"
+    }, {
+      id: 1,
+      img_url: "http://localhost:3000/img/banner2.png"
+    }, {
+      id: 1,
+      img_url: "http://localhost:3000/img/banner3.png"
+    }],
+
+
+    navlist: [{
+      id: 1,
+      img_url: "http://localhost:3000/img/icons/grid-01.png",
+      title: "美食"
+    }, {
+      id: 2,
+      img_url: "http://localhost:3000/img/icons/grid-02.png",
+      title: "洗浴"
+    }, {
+      id: 3,
+      img_url: "http://localhost:3000/img/icons/grid-03.png",
+      title: "结婚啦"
+    }, {
+      id: 4,
+      img_url: "http://localhost:3000/img/icons/grid-04.png",
+      title: "KTV"
+    }, {
+      id: 5,
+      img_url: "http://localhost:3000/img/icons/grid-05.png",
+      title: "找工作"
+    }, {
+      id: 6,
+      img_url: "http://localhost:3000/img/icons/grid-06.png",
+      title: "指导班"
+    }, {
+      id: 7,
+      img_url: "http://localhost:3000/img/icons/grid-07.png",
+      title: "汽车保养"
+    }, {
+      id: 8,
+      img_url: "http://localhost:3000/img/icons/grid-08.png",
+      title: "租房"
+    }, {
+      id: 9,
+      img_url: "http://localhost:3000/img/icons/grid-09.png",
+      title: "装修"
+    }, ]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-    
+  onLoad: function(options) {
+
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-    
+  onReady: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-    
+  onShow: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
-    
+  onHide: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
-    
+  onUnload: function() {
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
-    
+  onPullDownRefresh: function() {
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
-    
+  onReachBottom: function() {
+
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
-    
+  onShareAppMessage: function() {
+
   }
 })
